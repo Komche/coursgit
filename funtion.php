2c1b20656bb43cf27ef3a95e4bad84b59ff581b6
Komche <Adamoukomcheabdoulrazak@gmail.com>
2019-08-02 19:17:30 +0100

    insert du code php dans function.php qui est le second fichier créer

--- funtion.php
+++ funtion.php
@@ -1,2 +1,21 @@
 <?php
 
+function get_var_name($var)
+{
+    foreach ($GLOBALS as $var_name => $value) {
+        if ($value === $var) {
+            return $var_name;
+        }
+    }
+    return false;
+}
+
+function extract_get($index)
+{
+    if (!empty($_GET[$index])) {
+        return $GLOBALS[$index] = $_GET[$index];
+    } else {
+        return $GLOBALS[$index] = null;
+    }
+}
+
